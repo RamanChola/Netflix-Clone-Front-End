18df1bcd0c2ad3a5fe6be1c23377dfe3705fb437
RamanChola <ramanchola3@gmail.com>
2021-09-02 11:00:52 +0530

    https

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
       <div className="container">
         <div className="left">
           <img
-            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
           <Link to="" className="link">
